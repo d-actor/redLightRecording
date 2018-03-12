72fae1fabb0e4c53def9ea7a0021bcb098c27c04
Dan Actor <danltactor@gmail.com>
2018-03-12 12:29:25 -0600

    init samples component

--- client/src/components/Samples.js
+++ client/src/components/Samples.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {
   Header,
   Segment,
+  Grid,
+  Container,
 } from 'semantic-ui-react';
 
 class Samples extends Component {
@@ -10,10 +12,49 @@ class Samples extends Component {
     return(
       <Segment basic>
         <br />
-        <Header inverted as='h1' textAlign='center'>Samples</Header>
+        <Header inverted as='h1' textAlign='center'>Selected Works</Header>
+        <Container>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <iframe style={styles.iframe} src="https://bandcamp.com/EmbeddedPlayer/album=187857787/size=small/bgcol=ffffff/linkcol=0687f5/transparent=true/" seamless><a href="http://gr-ss.bandcamp.com/album/naysayer">Naysayer by grass</a></iframe>
+              </Grid.Column>
+              <Grid.Column width={2} />
+              <Grid.Column width={6}>
+                <Header as='h2' inverted>grass - Naysayer EP (2014)</Header>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <iframe style={styles.iframe} src="https://bandcamp.com/EmbeddedPlayer/album=1868496263/size=small/bgcol=ffffff/linkcol=0687f5/transparent=true/" seamless><a href="http://dustbloom.bandcamp.com/album/keeping-the-black-dog-at-bay">Keeping the Black Dog at Bay by Dustbloom</a></iframe>              
+              </Grid.Column>
+              <Grid.Column width={2} />
+              <Grid.Column width={6}>
+                <Header as='h2' inverted>Dustbloom - Keeping The Black Dog At Bay (2014)</Header>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <iframe style={styles.iframe} src="https://bandcamp.com/EmbeddedPlayer/album=1868496263/size=small/bgcol=ffffff/linkcol=0687f5/transparent=true/" seamless><a href="http://dustbloom.bandcamp.com/album/keeping-the-black-dog-at-bay">Keeping the Black Dog at Bay by Dustbloom</a></iframe>              
+              </Grid.Column>
+              <Grid.Column width={2} />
+              <Grid.Column width={6}>
+                <Header as='h2' inverted>Bird Watcher - Keeping The Black Dog At Bay (2014)</Header>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
       </Segment>
     )
   }
+}
+
+const styles = {
+  iframe: {
+    border: '0',
+    width: '100%',
+    height: '42px',
+  },
 }
 
 export default Samples;
